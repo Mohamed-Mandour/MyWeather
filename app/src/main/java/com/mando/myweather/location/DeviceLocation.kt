95522d99b0990d49eccda9c76d04b7df871958b6
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2020-04-25 23:35:07 +0100

    implement location

---
+++ app/src/main/java/com/mando/myweather/location/DeviceLocation.kt
@@ -0,0 +1,13 @@
+package com.mando.myweather.location
+
+import android.location.Location
+
+class DeviceLocation(location: Location) {
+    var latitude = 0.0
+    var longitude = 0.0
+
+    init {
+        latitude = location.latitude
+        longitude = location.longitude
+    }
+}
