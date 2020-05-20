49f3276378e085282c43a656d7065e1ec25fc503
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2020-05-20 02:35:17 +0100

    implement MVP

--- app/src/main/java/com/mando/myweather/mvp/CurrentScreenPresenter.kt
+++ app/src/main/java/com/mando/myweather/mvp/CurrentScreenPresenter.kt
@@ -16,17 +16,39 @@ class CurrentScreenPresenter(private val view: CurrentScreenContract.View) :
         view.showTimezone(current?.timezone)
         view.showSummary(current?.summary)
         view.showWeatherIcon(getWeatherIcon(current))
-        view.showLocationIcon(context?.resources?.getDrawable(R.drawable.ic_signs))
-        view.showDegreeIcon(context?.resources?.getDrawable(R.drawable.ic_celsius_degrees_symbol_of_temperature))
-        setHorizontalLine()
+        view.showLocationIcon(getLocationIcon())
+        view.showDegreeIcon(getDegreeIcon())
+        view.showPrecipitationIcon(getPrecipitationIcon())
+        view.showPrecipitationText(getPrecipitationText())
+        view.showPrecipitationValue(current?.precipProbability.toString())
+        view.showWindIcon(getWindIcon())
+        view.showWindText(getWindText())
+        view.showWindValue("${current?.windSpeed.toString()} km/h")
+        view.showPressureIcon(getPressureIcon())
+        view.showPressureText(getPressureText())
+        view.showPressureValue(current?.pressure.toString())
+        getHorizontalLineColor()
     }
 
-    private fun setHorizontalLine() {
-        context?.resources?.getColor(R.color.main_color)?.let { view.showWhiteLine(it) }
-    }
+    private fun getPressureText(): String? = context?.resources?.getString(R.string.pressure)
 
-    private fun getWeatherIcon(current: Current?): Drawable? {
-        return current?.getIcon?.let { context?.resources?.getDrawable(it) }
-    }
+    private fun getPressureIcon(): Drawable? = context?.resources?.getDrawable(R.drawable.ic_pressure)
+
+    private fun getWindText(): String? = context?.resources?.getString(R.string.wind)
+
+    private fun getWindIcon(): Drawable? = context?.resources?.getDrawable(R.drawable.ic_fan)
+
+    private fun getLocationIcon() = context?.resources?.getDrawable(R.drawable.ic_signs)
+
+    private fun getDegreeIcon() = context?.resources?.getDrawable(R.drawable.ic_celsius_degrees_symbol_of_temperature)
+
+    private fun getPrecipitationText() = context?.resources?.getString(R.string.precipitation)
+
+    private fun getPrecipitationIcon(): Drawable?  =  context?.resources?.getDrawable(R.drawable.ic_precipitation)
+
+    private fun getHorizontalLineColor() = context?.resources?.getColor(R.color.main_color)?.let { view.showWhiteLine(it) }
+
+    private fun getWeatherIcon(current: Current?): Drawable? = current?.getIcon?.let { context?.resources?.getDrawable(it) }
+
 }
 
