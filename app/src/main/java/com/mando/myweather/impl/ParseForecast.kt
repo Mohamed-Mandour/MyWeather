48363925f733fad7ef52d4b8e627a7bd1fe61346
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2020-05-27 21:54:38 +0100

    populate the hourly date class

--- app/src/main/java/com/mando/myweather/impl/ParseForecast.kt
+++ app/src/main/java/com/mando/myweather/impl/ParseForecast.kt
@@ -1,7 +1,10 @@
 package com.mando.myweather.impl
 
-import com.mando.myweather.model.*
+import com.mando.myweather.model.Current
+import com.mando.myweather.model.Forecast
+import com.mando.myweather.model.Hourly
 import org.json.JSONObject
+
 private const val TIMEZONE = "timezone"
 private const val CURRENTLY = "currently"
 private const val ICON = "icon"
@@ -14,33 +17,56 @@ private const val WINDSPEED = "windSpeed"
 private const val VISIBILITY = "visibility"
 private const val PRECIPPROBABILITY = "precipProbability"
 private const val CLOUDCOVER = "cloudCover"
+private const val HOURLY = "hourly"
+private const val DATA = "data"
 
 class ParseForecast(private val forecastJson: String?) {
 
-    fun parseForecastJson(): Forecast{
+    fun parseForecastJson(): Forecast {
         val forecast = Forecast()
         forecast.setCurrent(mapCurrent(forecastJson))
+        forecast.setHourly(mapHourly(forecastJson))
         return forecast
     }
 
     private fun mapCurrent(forecastJson: String?): Current? {
-        val current = Current()
-
         val forecast = JSONObject(forecastJson)
-        val timezone: String = forecast.getString(TIMEZONE)
         val currently: JSONObject = forecast.getJSONObject(CURRENTLY)
-        current.timezone = timezone
-        current.icon  = currently.getString(ICON)
-        current.time = currently.getLong(TIME)
-        current.summary = currently.getString(SUMMARY)
-        current.temperature = currently.getString(TEMPERATURE)
-        current.humidity = currently.getDouble(HUMIDITY)
-        current.pressure = currently.getDouble(PRESSURE)
-        current.windSpeed = currently.getDouble(WINDSPEED)
-        current.visibility = currently.getDouble(VISIBILITY)
-        current.precipProbability = currently.getDouble(PRECIPPROBABILITY)
-        current.cloudCover = currently.getDouble(CLOUDCOVER)
-        return current
+        return Current(
+            forecast.getString(TIMEZONE),
+            currently.getString(ICON),
+            currently.getLong(TIME),
+            currently.getString(SUMMARY),
+            currently.getString(TEMPERATURE),
+            currently.getDouble(HUMIDITY),
+            currently.getDouble(PRESSURE),
+            currently.getDouble(WINDSPEED),
+            currently.getDouble(VISIBILITY),
+            currently.getDouble(PRECIPPROBABILITY),
+            currently.getDouble(CLOUDCOVER)
+        )
+    }
+
+
+    private fun mapHourly(forecastJson: String?): Array<Hourly?> {
+        val forecast = JSONObject(forecastJson)
+        val hourly: JSONObject = forecast.getJSONObject(HOURLY)
+        val data = hourly.getJSONArray(DATA)
+        val hours: Array<Hourly?> = arrayOfNulls(data.length())
+
+        for (i in 0 until data.length()) {
+            val jsonHour = data.getJSONObject(i)
+            hours[i] = Hourly(
+                jsonHour.getString(ICON),
+                jsonHour.getLong(TIME),
+                jsonHour.getString(SUMMARY),
+                jsonHour.getDouble(TEMPERATURE),
+                jsonHour.getDouble(PRECIPPROBABILITY),
+                jsonHour.getDouble(HUMIDITY),
+                jsonHour.getDouble(WINDSPEED)
+            )
+        }
+        return hours
     }
 
 }
