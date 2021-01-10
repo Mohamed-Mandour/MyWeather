4d082a8d0a021e8c4f2051ec1ac57092723b432b
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-01-10 20:08:53 +0000

    impl getLastLocation()

--- app/src/main/java/com/mando/weatherforecast/repository/ForecastRepositoryImpl.kt
+++ app/src/main/java/com/mando/weatherforecast/repository/ForecastRepositoryImpl.kt
@@ -3,10 +3,12 @@ package com.mando.weatherforecast.repository
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.mando.weatherforecast.location.DeviceLocation
 import com.mando.weatherforecast.location.FusedLocationDataStore
 import com.mando.weatherforecast.location.LocationDataStore
 import com.mando.weatherforecast.model.CurrentResponse
 import com.mando.weatherforecast.model.ForecastResponse
+import com.mando.weatherforecast.model.HourlyResponse
 import com.mando.weatherforecast.network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,13 +18,13 @@ import timber.log.Timber
 class ForecastRepositoryImpl(private val context: Context) : ForecastRepository {
 
     private val retrofitClient = RetrofitClient()
-    private val locationDataStore: LocationDataStore?
-        get() = FusedLocationDataStore.getInstance(context)
 
-    override fun getSavedCurrentForecast(): LiveData<CurrentResponse> {
+    private val deviceLocation = FusedLocationDataStore.getLastLocation()
+
+    override fun getCurrentForecast(): LiveData<CurrentResponse> {
         val data = MutableLiveData<CurrentResponse>()
 
-        val forecast = retrofitClient.getForecast(locationDataStore?.location)
+        val forecast = retrofitClient.getForecast(deviceLocation)
 
         forecast.enqueue(object : Callback<ForecastResponse> {
             override fun onResponse(
@@ -32,7 +34,6 @@ class ForecastRepositoryImpl(private val context: Context) : ForecastRepository 
                 if (response.isSuccessful) {
                     data.value = (response.body()?.currently)
                     Timber.d("response.isSuccessful: $response")
-
                     Timber.d("response.isSuccessful: ${data.value}")
                 }
             }
@@ -41,13 +42,12 @@ class ForecastRepositoryImpl(private val context: Context) : ForecastRepository 
             }
 
         })
-        Timber.d("data:: ${data.value?.summary}")
         return data
     }
 
     override fun getTimeZone(): LiveData<String> {
         val timezone = MutableLiveData<String>()
-        val forecast = retrofitClient.getForecast(locationDataStore?.location)
+        val forecast = retrofitClient.getForecast(deviceLocation)
         forecast.enqueue(object : Callback<ForecastResponse> {
             override fun onResponse(
                 call: Call<ForecastResponse>,
@@ -58,11 +58,37 @@ class ForecastRepositoryImpl(private val context: Context) : ForecastRepository 
                     Timber.d("response.isSuccessful: $response")
                 }
             }
+
             override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
                 Timber.d("onFailure: $t")
             }
 
         })
         return timezone
+    }
+
+    override fun getHourlyForecast(): LiveData<HourlyResponse> {
+        val data = MutableLiveData<HourlyResponse>()
+
+        val forecast = retrofitClient.getForecast(deviceLocation)
+
+        forecast.enqueue(object : Callback<ForecastResponse> {
+            override fun onResponse(
+                call: Call<ForecastResponse>,
+                response: Response<ForecastResponse>
+            ) {
+                if (response.isSuccessful) {
+                    data.value = (response.body()?.hourly)
+                    Timber.d("response.isSuccessful: $response")
+                    Timber.d("response.isSuccessful hourly Summary : ${data.value?.summary}")
+                }
+            }
+
+            override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
+                Timber.d("onFailure HourlyForecast : $t")
+            }
+
+        })
+        return data
     }
 }
