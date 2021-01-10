7b4ba4097a6bd79d949528455603a86e01e4ebaf
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-01-10 20:09:24 +0000

    add hourlyList

--- app/src/main/java/com/mando/weatherforecast/fragments/HourlyFragment.kt
+++ app/src/main/java/com/mando/weatherforecast/fragments/HourlyFragment.kt
@@ -5,97 +5,71 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.mando.weatherforecast.R
-import com.mando.weatherforecast.model.HourlyResponse
-import kotlinx.android.synthetic.main.daily_recycle_item.view.*
-import kotlinx.android.synthetic.main.fragment_hourly.view.*
-import timber.log.Timber
+import com.mando.weatherforecast.adpater.HourlyAdapter
+import com.mando.weatherforecast.utils.inflate
+import com.mando.weatherforecast.viewModel.CurrentViewModel
+import com.mando.weatherforecast.viewModel.CurrentViewModelFactory
+import com.mando.weatherforecast.viewModel.HourlyViewModel
+import com.mando.weatherforecast.viewModel.HourlyViewModelFactory
+import kotlinx.android.synthetic.main.fragment_hourly.*
 
 class HourlyFragment : Fragment() {
 
-    var hourlyList: Array<HourlyResponse?>? = null
+    private lateinit var hourlyViewModel: HourlyViewModel
+    private lateinit var currentViewModel: CurrentViewModel
+
+    private var adapter = HourlyAdapter()
+
+
+    companion object {
+        fun newInstance(): HourlyFragment {
+            return HourlyFragment()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
-//        FetchForecastTask(this).execute()
+        val context = activity?.applicationContext
+        if (context != null) {
+            val hourlyViewModelFactory = HourlyViewModelFactory(context)
+            val currentViewModelFactory = CurrentViewModelFactory(context)
+            hourlyViewModel =
+                ViewModelProviders.of(this, hourlyViewModelFactory).get(HourlyViewModel::class.java)
+            currentViewModel = ViewModelProviders.of(this, currentViewModelFactory)
+                .get(CurrentViewModel::class.java)
+            getHourlyForecast()
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val root = inflater.inflate(R.layout.fragment_hourly, container, false)
-        with(root.recyclerView) {
-            if (isAdded)
+        return container?.inflate(R.layout.fragment_hourly)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            if (isAdded){
                 recyclerView.layoutManager = LinearLayoutManager(activity)
-                recyclerView.adapter = HourlyRecyclerViewAdapter(hourlyList)
+                recyclerView.adapter = adapter
         }
-        return root
     }
 
-    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(hourly: HourlyResponse?) {
-            Timber.d("HourlyRecyclerViewAdapter ViewHolder called()")
-            view.time.text = hourly?.time.toString()
-        }
-
-    }
-
-    class HourlyRecyclerViewAdapter(private val hourlyList: Array<HourlyResponse?>?) :
-        RecyclerView.Adapter<com.mando.weatherforecast.adpater.HourlyRecyclerViewAdapter.ViewHolder>() {
-
-        override fun onCreateViewHolder(
-            parent: ViewGroup,
-            viewType: Int
-        ): com.mando.weatherforecast.adpater.HourlyRecyclerViewAdapter.ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.daily_recycle_item, parent, false)
-            return com.mando.weatherforecast.adpater.HourlyRecyclerViewAdapter.ViewHolder(view)
-        }
-
-        override fun getItemCount(): Int {
-            return hourlyList?.size ?: 0
-        }
-
-        override fun onBindViewHolder(
-            holder: com.mando.weatherforecast.adpater.HourlyRecyclerViewAdapter.ViewHolder,
-            position: Int
-        ) {
-            holder.bind(hourlyList?.get(position))
-        }
+    private fun getHourlyForecast() {
+        var timeZone = ""
+        currentViewModel.getTimeZone().observe(this, Observer { timeZoneObserver ->
+            timeZone = timeZoneObserver
+        })
+        hourlyViewModel.getHourlyForecast().observe(this, Observer { hourly ->
+            adapter.setHourlyForecast(hourly.data, timeZone)
+        })
     }
 }
-//
-//private class FetchForecastTask(val fragment: HourlyFragment) :
-//    AsyncTask<Void?, Void?, String>() {
-//
-//    private val weakReference: WeakReference<HourlyFragment> =
-//        WeakReference(fragment)
-//
-//    override fun doInBackground(vararg params: Void?): String? {
-//        return weakReference.let {
-//            fragment.activity.let { it1 ->
-//                it1?.let { it2 ->
-//                    ForecastJson(
-//                        it2
-//                    )
-//                        .getForecastJson()
-//                }
-//            }
-//        }
-//    }
-//
-//    override fun onPostExecute(result: String?) {
-//        super.onPostExecute(result)
-//        val parseForecast = ParseForecast(result)
-//        val forecast = parseForecast.parseForecastJson()
-//        val hourly: Array<Hourly?>? = forecast.getHourly()
-//        fragment.hourlyList = hourly
-//    }
-//}
 
 
