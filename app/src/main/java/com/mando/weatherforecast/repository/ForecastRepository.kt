2ae8ec7803f856d148899c3e73768c1993f6a52b
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-01-10 20:06:32 +0000

    add fragment newInstance

--- app/src/main/java/com/mando/weatherforecast/repository/ForecastRepository.kt
+++ app/src/main/java/com/mando/weatherforecast/repository/ForecastRepository.kt
@@ -2,12 +2,15 @@ package com.mando.weatherforecast.repository
 
 import androidx.lifecycle.LiveData
 import com.mando.weatherforecast.model.CurrentResponse
+import com.mando.weatherforecast.model.HourlyResponse
 
 
 interface ForecastRepository {
 
-    fun getSavedCurrentForecast(): LiveData<CurrentResponse>
+    fun getCurrentForecast(): LiveData<CurrentResponse>
 
     fun getTimeZone(): LiveData<String>
+    
+    fun getHourlyForecast(): LiveData<HourlyResponse>
 
 }
